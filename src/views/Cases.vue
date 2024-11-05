477569fca96eb4654e8be50b30f83db35c4329ba
nicolascunn <nicolascunn@gmail.com>
2024-11-05 22:39:22 +0100

    Update Cases.vue

--- src/views/Cases.vue
+++ src/views/Cases.vue
@@ -23,7 +23,7 @@
 
         <item-case
           name="GetProud.io (2022)"
-          img="https://mhvnobgazgjwiuroumjw.supabase.in/storage/v1/object/public/static/preview-min.png"
+          img="../assets/cases-case-proud-ui.png"
           title="Designing and Developing an MVP in 2 months including UX Research"
           :tags="['UX Research', 'Design', 'Development', 'Web3/Crypto']"
           desc="In January 2022 I started building the MVP for Proud a team recognition software platform that allows teams to reward their colleagues with NFTs."
